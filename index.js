8f9e071c64d49f4b47181ef567bece83390d94bd
felixo2022 <walterbarax@gmail.com>
2022-08-23 04:51:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+let multiply; 
+let num1 = 31;
+let num2;
+num2 = num1 - 29;
+multiply = num1 * num2;
+let random = 31;
+let mod;
+let num3 = 11;
+let num4 = 7;
+mod = num3 - num4;
+let max;
+max = 20;
